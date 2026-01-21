1a58b825571935352bec1d93194d1c5434fa8793
prayer-on <prayer.onaiwu@gmail.com>
2026-01-21 18:31:10 +0100

    detail page desktop completed

--- src/Views/Detail.js
+++ src/Views/Detail.js
@@ -20,17 +20,27 @@ const allImages = [...new Set([property.cover, ...property.pictures])];
     return(
         <>
         <Header/>
+        <section className="detail-section">
         <DetailBanner imageUrls={allImages}/>
+        <div className="info-box">
+        <div>
         <Location property={property} 
                   location={property.location}/>
         {property.tags.map((item, index) => <Button key={index} name={item}/>)}
+        </div>
+        <div className="user-rank-box">
+        <Host host={property.host}/>
+        <Star rating={property.rating}/>
+        </div>
+        </div>
+        <div className="smfilter-box">
         <SmFilter title= "Description" content={property.description}/>
         <SmFilter title= "Amenities" content={property.equipments.map((item, index) =>
             <li key={index}>
             {item}
             </li>)}/>
-        <Host host={property.host}/>
-        <Star rating={property.rating}/>
+        </div>
+        </section>
         <Footer/>
         </>
         
